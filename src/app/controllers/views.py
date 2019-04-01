dd73789da61b746e4afe77b60a5f4d886aeecb97
jwellington58 <wj070996@gmail.com>
2019-04-01 16:10:16 -0300

    Crud de usuario

--- src/app/controllers/views.py
+++ src/app/controllers/views.py
@@ -1,6 +1,62 @@
-from flask import Flask
-from app import app
+from flask import Flask, request, jsonify, abort
+from app import app, db
+from app.models.tables import Usuario, Remessa, Local
 
-@app.route("/")
-def hello():
-    return "Hello!"
+@app.route('/usuario', methods=['GET'])
+def get_user():
+    if request.method == 'GET':
+        users = []
+        userRes = Usuario.query.all()
+        for us in userRes:
+            user = {
+                'id': us.id,
+                'username': us.username,
+                'senha': us.senha,
+            }
+            users.append(user)
+       
+        return jsonify({'usuarios': users})
+
+
+@app.route('/usuario', methods=['POST'])
+def create_user():
+    user = {
+        'username': request.json['username'],
+        'senha': request.json['senha']
+    }
+    username = user['username']
+    senha = user['senha']
+    #print('UserName {}'.format(username))
+    u = Usuario(username, senha)
+
+    db.session.add(u)
+    db.session.commit()
+
+    return jsonify({'usuario': user}), 201
+
+
+@app.route('/user/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    user = {
+        'username': request.json['username'],
+        'senha': request.json['senha']
+    }
+    userRes = Usuario.query.filter_by(id=user_id).first_or_404()
+    if userRes == None:
+        abort(404)
+    userRes.username = user['username']
+    userRes.senha = user['senha']
+
+    db.session.commit()
+
+    return jsonify({'usuario': user}), 204
+
+
+@app.route('/user/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    userRes = Usuario.query.filter_by(id=user_id).first_or_404()
+    if userRes == None:
+        abort(404)
+    db.session.delete(userRes)
+    db.session.commit()
+    return jsonify({'result': True})
