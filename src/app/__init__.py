cbeb71429de652e90cab036b0339c1fc1bfbe2cc
Weskley Damasceno <weskleydamasceno@gmail.com>
2019-04-01 15:41:40 -0300

    Mudanças de settings

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -8,6 +8,9 @@ db = SQLAlchemy(app)
 
 app.config.from_object('settings')
 
+from app.controllers import views
+from app.models import tables
+
 # For development (show all settings for flask app)
 for key in app.config:
     print(key, app.config[key])
--- src/settings.py
+++ src/settings.py
@@ -1,7 +1,17 @@
 #
 # Configurações da aplicação
 #
-Debug = True
+
+import os
+from os.path import abspath
+
+DEBUG = True
+
+# diretório base
+basedir = os.path.abspath(os.path.dirname(__name__))
+
+# diretório base da aplicação
+BASE_DIR = basedir
 
 # connection string: mysql://usuario:senha@host/nomedobanco
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/dbdevweb'
